55fcaacf3d33344a9fad001cdbfb070bd213b1d1
J-max <joelmtn7@gmail.com>
2025-03-08 04:16:43 +0100

    first eccomerce

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let cartCount = 0;
+    const cartCountElement = document.createElement("span");
+    cartCountElement.classList.add("cart-count");
+    document.querySelector(".cart").appendChild(cartCountElement);
+    
+    const buttons = document.querySelectorAll(".add-to-cart");
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            cartCount++;
+            cartCountElement.textContent = cartCount;
+            alert("Item added to cart!");
+        });
+    });
+});
+
+const images = [
+    'shoes/IMG-20250304-WA0022.jpg',
+    'shoes/IMG-20250304-WA0034.jpg',
+    'shoes/IMG-20250304-WA0030.jpg',
+    
+];
+
+let currentIndex = 0;
+
+const banner = document.querySelector('#home');
+function changeBackground() {
+    banner.style.transform = "translateX(-100%)";
+
+    setTimeout( () => {
+    banner.style.backgroundImage = `url(${images[currentIndex]})`;
+    banner.style.opacity = 0.8;
+    banner.style.transform = "translateX(0)";
+   
+
+    setTimeout( () => {
+        banner.style.opacity = 1;
+        banner.style.transform = "translateX(0)";
+    }, 300); 
+}, 900);
+
+    currentIndex = (currentIndex + 1) % images.length;
+}
+
+setInterval(changeBackground, 3000)
+changeBackground();
